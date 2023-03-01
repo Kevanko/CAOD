53049449b133c3d77f595acbacb25cf7d1ebcc10
Misha <mr_vidali@mail.ru>
2023-03-01 22:43:29 +0700

    Some changies

--- sort/functions.c
+++ sort/functions.c
@@ -4,6 +4,12 @@ int getrand(int min, int max) {
   return (double)rand() / (RAND_MAX + 1.0) * (max - min) + min;
 }
 
+double wtime() {
+  struct timeval t;
+  gettimeofday(&t, NULL);
+  return (double)t.tv_sec + (double)t.tv_usec * 1E-6;
+}
+
 // radixSort
 //  Функция для поиска максимального числа в массиве
 int getMax(int arr[], int n) {
